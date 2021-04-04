e191ff5e84372d3c617301f3e13e445d203798e9
Simen Kristoffersen <lissomliksom@gmail.com>
2021-04-04 15:36:54 +0200

    Add files for basic prototype

---
+++ main.js
@@ -0,0 +1,168 @@
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXK0OkxxddooooooodddxkO00XNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxdlc;,'.......................',;:loxOKNWMMMMMMMMMMMMN0OXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0xoc,.........''''',,,,,,,,,,,,,'','..........,:lxOXWMMMMMMWk,'dWMMMMMMMMMMMMMX0KWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKko:'.....',;;::cccccccccccccccccccccccoc....,,,'.......':lxKNMMMN00NMMMMMN0xkXMMMNl.:0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOo:.....';:cllllcccccccccccccccccccccccccl:'...,:c::::;;,'......;lkKWMMMMMMMWx...lNMMWKk0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxc,....,:looollcccccccccccccccccccccccccccc:'....,::::::::::::;,'... .'cxKWMMMMW0l;cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXkc'...';ldddolcccccccccccccccccccccccccc:::;,.....';:::::::::::::::::;,'.....:xKWMMMWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0l,...':oxxdlcccccccccccccccccccccccccc:;,'........,::::::::::::::::::::::::;'....'lONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNk:... .ckkdolcccccccccccccccccccccccc::,'........',;:c:::::::::::::::::::::::::::;'....;xXWMMMMMMMNOk0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXx;..... .cxocccccccccccccccccccccccccc;'......',;;::::::::::::::::::::::::::::::::::::,....,dKWMMMMNo...xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXx;...,lo,...;cllllcccccccccccccccccccc:,.... .,:::::::::::::::::::::::::::::::::::::::::::;'...,dXWMMWOc,cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNk;...,lxkxc'.....,;colcccccccccccccccccc;.';'. .,:::::::::::::::::::::::::::::::::::::::::::::;'...,xNMMMWNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0l...'lxkxdlc:;'......'cllcccccccccccccccc:;:c:....,::::::::::::::::::::::::::::::::::::::::::::::;....:OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx,...:dkxdlc:::c:::;'....;llcccccccccccccccccccc;. ..;:c:::::::::::::::::::::::::::::::::::::::::::::,...'dXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKl...,oxkxoc::::::::::::'. .,llcccccccccllllllcccc:'. .,::::::::::::::::::::::::::::::::::::::::::::::::;'...:0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO;...cdkkxoc::::::::::::c:'. .;llccccllc:;;;;:cllllc'. .,::::::::::::::::::::::::::::::::::::::::::::::::::,...,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk,..'lxkxdlc::::::::::::::::'...':llc:;'.........',,....'::::::;,,,;:::::::::::::::::::::::::::::::::::::::::;....dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'..,oxkxdlc::::::::::::::::::,............',,'.........,::::::,.. ...,::::::::::::::::::::::::::::::::::::::::;....dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk'..,okkxdlc::::::::::::::::::::;,'.....',;:::::::;;,,;;::::::::,.......;::::::::::::::::::::::::::::::::::c::::::'. .dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO,..'lollc;,,;:::c:::::::::::::::c::::::::::::::::::::::::::::::::,......;:::::::::::::::::::::::::::::::::;;:::::::'...xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0;...............,;::::c:::::::::::::::::::::::::::::::::::::::::::::;,,;::ccccccc:::::::;;;;;,,,;:::::::::,..;c:::::;...'kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc.  ...';;,,,,.....,:::::::::::::::::::::::::::::::::::::::::::::::::::::::cclllllc::::,...........;:::::::'..;::::c::;...:KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx.  ..:oxxocclllc'. .,::::::::::::::::::::::::::::::::::::::::::::::::::::::cllllllc::c;.  .;:;... ..';::::,...;::::::::,...oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;...,okkkdcccccclc. ..:::::::::::::::::::::::::::::::::::::::::::::::::::::::cccccc::::;. .cXW0; ......';:;...'::::::::::'..'OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd. .,dkkkxlccccccll'...:c::::::::::::::::::::::::::::::::::::::::::::::::::'.,::::::::::,. .oWMO' .';'.........;::::::::::,...lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK:..'okkkkdccccccclc....:c:::::::::::::::::::::::::::::::::::::::::::::::::;. .;c::::::::'. 'kMWd. .,c:;'.. . .,::::::;..;::...,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk' .:xkkkkocccccccll'...,;::::::::::::::::::::::;,',;::::::::::;::::::::::;.. .;::::::::;. .:XMK;...:cccc,....,::::::,..':::,. .xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd. .lkkkkxocccccccclc'....',:::::::::::::::::::,......,:::::::'.,::;,''''... .':::::::::'. .xWWd. .,:ccc:....,::c::,...':::c;...lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl. 'okkkkxlcccccccccll:,.....;:::::::::::::::::,.. . ..;::::::,.,:::,.......';:::::::::,. .cXWO,...:ccc:'...;:::;,....,:::::;...:KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX:..,dkkkkxlccccccccccclll:.. .;:::::::::::::::::,.....';:::ccc:;;:::::;;,;;:::::::::::;...;0W0;. .;cccc;. ..,,'...  ..,;:::c:'..,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;..,xkkkkxlccccccccccccccl;. .,:::::::::::::::::::;;,;::::cclllccc::::::::::::::::::c;.. .o0k;...,:cccc:'.........'.....';:::'. ,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;..,xkkkkxlccccc::,,''''... ..;::::::::::::::::::::::::::cclllllcc::::::::::::::::::,... ..... .,:cccccc:;,'',,;;:c:;.. ..:::'. ,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX:..,dkkkkxlccc:;'...........';::::::::::::::::::::::::::::ccllllc::::::::::::::::;'......,,..  ..;:cccccccccccccccccc:'. .,::'. ,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl. 'okkkkkocc:,..  .',,,,;;::::::::::::::::::::::::::::::::ccccc::::::::::::::;,........:xc. ... .';cccccccccccccccccc,. .,:;...:KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd. .lkkkkkdc:,.'....:::::::::::::::::::::::::::::::::::::::::::::::::::::::;'.....;dd,. .....';'....,:ccccccccccccccc:,. .,:;. .lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk' .:kkkkkxl:';c,. .;:::::::::::::::::::::::::::::::::::::::::::::::c:::;'.....;oOKx;.   ..':cc:;.....,:ccccccccccccc:'. .,:,. .dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK:..,dkkkkkoc:cl;. .,:::::::::::::::::::::::::::::::::::::::::::::::;'......:d0NKd;. ..'....':ccc:;.. ..;:ccccccccccc;. ..;:.. ,0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd...ckkkkkxlclo;. .,:::::::::::::::::::::::::::::::::::::::::;,'.......,lxKNXkl'....,:c:,....':ccc:,....';ccccccccc:'. .,:,. .lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;..,dkkkkkdlc;....;:::::::::::::::::::::::::::::::::::::::;.......,cd0NWXkl,....';:ccccc:,....':ccc:'....':cccccc:,. ..;:'..'OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx. .:xkOko;'....';::::::::::::::::::::::::::::::::::::c:::;.. .oO0NWN0xc,....',:cccccccccc:,....':ccc;'....,:cccc,....;:,...oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc...lOx;.....';:::::::::::::::::::::::::::::::::::::::::::,. .oK0xl;.....',::cccccccccccccc:,.. .':cc:;.....;::,....;:;...;KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0;..,l;...:olc::::::::::::::::::::::::::::::::::::::::::::,. .........';:cc::;;;:ccccccccccc::,....':cc:,....... ..;:;.. 'kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk'.... .:xxdl::::::::::::::::::::::::::::::::::::::::::::,..  ...,:cccc:;'......,:cccccccccccc:,....':cc:,..  ..':::'. .dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'. ..'okkxdl::::::::::::::::::::::::::::::::::::::::::::,..  ..;clc;'..........;cccccccccccccc:,....':cc;.. .,:::'...oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'. .:xkkkxdlc::::::::::::::::::::::::::::::::::::::::::::,.............';;'. .,cccccccccccccccc:,....';'....;:;'...oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk,..'lxkkkkxoc::::::::::::::::::::::::::::::::::::::::c:::,'..   ..',;:::;....;cccccccccccccccccc:,.......';:;....dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO;...cxkkkkxdlc:::::::::::::::::::::::::::::::::::::;,'........';:::::::'. .,:ccccccccccccccccccc;.. ..,;::,...,kNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKc. .;oxkkkkxoc::::::::;'.'',;;;:::::::::::;;;,,'.........',;::::::::;'. .,:cccccccccccccccccc:,....'::::'...:0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx'...cdxkkkxdlc:::::::;,'..........................',;::::::::::::,....,ccccccccccccccccccc;'....,:::,....oXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0c...,lxkkkkxdlc::::::::::;,,''...........''',;;:::::::::::::::;'.. .'clllccccccccccccc::'....';::;....:OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNk;...,lxkkkxxdlc::::::::::::::::::::::::::::::::::::::::::::,..... ..;clllccccccccc:;'.....;::;'...,xXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXx,...,lxxxkkxdocc:::::::::::::::::::::::::::::::::::::::,.....,,.. ..;llllcccc:;,......;::;'...'oKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd,...':ldxkkxxdlcc:::::::::::::::::::::::::::::::c:;'.....,::c:,.....;cc:;,'......';::,....,oKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXx:.....';;::::;,''''',,;::::::::::::::::::c:::;,.....';::::::::,.. ..........';::;'....;dKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNOl'. .    .............';:::::::::::::::;,'.....',:::::::::::::,......',;;::;'....'ckNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXxc'.  ...',,,,;;,'.....,:::::::::::;'....'';::::::::::::c:::::::::::::,'.....:dKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxc'.....';::cclc;.....';:::::::::,,;::::::::::::::c:::::::::::;;'.....':d0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXko;......',;:cc:'....';clcccc::::::::::::::::::::cccc::;,'......,lxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNKko:'......',;,......;clodooooolllllllllllllc::;,'......';lx0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOxl:,.......... ...';:ccllccccc:::;;,''.......';ldOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxol:;,......  .................',:coxk0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkkxddoooooooooddxxkO0KXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+// SHA256-hashing is not a part of JS.
+// Install crypto-js with NPM and import it.
+const SHA256 = require('crypto-js/sha256')
+
+
+class Block {
+    // Set up the basic values of the block.
+    // Data can contain various data, i.e. sender, receiver, amount...
+
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index
+        this.timestamp = timestamp
+        this.data = data
+        this.previousHash = previousHash
+        this.hash = this.calculateHash()
+    }
+
+    calculateHash() {
+        const hashBasicInfo = this.index + this.previousHash + this.timestamp
+        const hashData = JSON.stringify(this.data)
+        return SHA256(hashBasicInfo + hashData).toString()
+    }
+}
+
+class Blockchain {
+    // Initialize a chain: an array starting with a Genesis Block.
+    constructor() {
+        this.chain = [this.createGenesisBlock()]
+    }
+
+    // The first block in a chain is called a Genesis Block.
+    // Create this manually.
+    createGenesisBlock() {
+        // Add Index, timestamp and data. Set previous hash to 0.
+        const genesisData = [0, "04/04/2021", "LeseCoin Genesis Block", "0"]
+        return new Block(genesisData)
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock) {
+        // When creating a new block, get and add the hash of the previous block to it.
+        // This is to ensure that the new block is added at the end of the chain.
+        newBlock.previousHash = this.getLatestBlock().hash
+        // Calculate a new hash for our new block.
+        newBlock.hash = newBlock.calculateHash()
+        // Add our block to the end of our chain.
+        // Usually this is dome with various checks and validations, this is just a simple example.
+        this.chain.push(newBlock)
+    }
+
+    isChainValid() {
+        // Loop through all blocks in the entire chain to validate that every block is linked properly.
+        // Start at index 1 to exclude the Genesis Block.
+        for(let i = 1; i < this.chain.length; i++) {
+            const previousBlock = this.chain[i - 1]
+            const currentBlock = this.chain[i]
+
+            // Recalculate hash for our current block.
+            // If the hash of our current block does not correspond with the recalculated value, something is wrong.
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false
+            }
+
+            // If the value for previous hash in our current block does not correspond with the actual previous hash, something is wrong.
+            if(currentBlock.previousHash !== previousBlock.hash ) {
+                return false
+            }
+
+            // If there's no issue with either case above, validate that all blocks are linked properly.
+            return true
+        }
+    }
+}
+
+// Create a new chain and populate with a few blocks
+const createChain = () => {
+    let klodeCoin = new Blockchain()
+    klodeCoin.addBlock(new Block(1, "04/04/2021", { amount: 3}))
+    klodeCoin.addBlock(new Block(2, "04/04/2021", { amount: 5}))
+    klodeCoin.addBlock(new Block(3, "04/04/2021", { amount: 8}))
+    klodeCoin.addBlock(new Block(4, "04/04/2021", { amount: 13}))
+    console.log(JSON.stringify(klodeCoin, null, 4))
+}
+
+const corruptChain = () => {
+    // Validate  chain without corruption 
+    console.log(`Is blockchain valid before corruption? ${klodeCoin.isChainValid()}`)
+
+    // Corrupt chain data and revalidate
+    klodeCoin.chain[1].data = { amount: 100 }
+    console.log(`Is blockchain valid after corrupting data? ${klodeCoin.isChainValid()}`)
+
+    // Corrupt chain hash and revalidate
+    /*
+    Important note: This function does not currently work as intented, and is therefore commented out.
+    klodeCoin.chain[1].hash = klodeCoin.chain[1].calculateHash()
+    console.log(`Is blockchain valid after corrupting hash? ${klodeCoin.isChainValid()}`)
+    */
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# KlodeCoin
+
+A small project to better undestand OOP in JavaScript, practicing Git, how blockchain technology works, and writing and running scripts with Node.js.
+
+## Todo
+- Implement Proof-of-Work
+- Implement a function to add n blocks to chain with random data
+- Add script for adding n blocks to package.json
+- Add script for corrupting chain to package.json
+- Implement a decent date-library?
+
+## Credits
+[YouTube-series by Simply Explained](https://www.youtube.com/watch?v=zVqczFZr124&list=PLzvRQMJ9HDiTqZmbtFisdXFxul5k0F-Q4)
